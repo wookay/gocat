7f8b46afcc9fee60eba581265504de84b85fae8c
wookyoung <wookay.noh@gmail.com>
2015-10-28 17:06:14 +0900

    timezone

--- time_test.go
+++ time_test.go
@@ -19,5 +19,13 @@ func TestTimestamp(t *testing.T) {
 	assert.Equal(t, time.November, tm.Month())
 	assert.Equal(t, 11, int(tm.Month()))
 	assert.Equal(t, 1, tm.Day())
-	assert.Equal(t, "2015-11-01 20:00:00 +0900 KST", tm.String())
+	timezone, offset := tm.Zone()
+	if "KST" == timezone {
+		assert.Equal(t, 9*60*60, offset)
+		assert.Equal(t, "2015-11-01 20:00:00 +0900 KST", tm.String())
+	} else {
+		assert.Equal(t, "UTC", timezone)
+		assert.Equal(t, 0, offset)
+		assert.Equal(t, "2015-11-01 11:00:00 +0000 UTC", tm.String())
+	}
 }
