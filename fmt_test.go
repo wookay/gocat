101688839675abd88a6d3f0009789180e600e422
wookyoung <wookay.noh@gmail.com>
2015-11-05 16:37:43 +0900

    fmap

--- fmt_test.go
+++ fmt_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"math"
+	"strings"
 	"testing"
 )
 
@@ -14,4 +15,29 @@ func TestFormatted(t *testing.T) {
 func TestFormattedSlices(t *testing.T) {
 	a := [2]float64{math.Pi, math.Pi}
 	assert.Equal(t, "3.14,3.14", fmt.Sprintf("%.2f,%.2f", a[0], a[1]))
+}
+
+type Map interface {
+	fmap(f func(interface{}) string) []string
+}
+
+type Point [2]float64
+
+func (point Point) fmap(f func(interface{}) string) []string {
+	arr := make([]string, len(point))
+	for idx, value := range point {
+		arr[idx] = f(value)
+	}
+	return arr
+}
+
+func TestPoint(t *testing.T) {
+	point := [2]float64{math.Pi, math.Pi}
+
+	sprint_float := func(value interface{}) string {
+		return fmt.Sprintf("%.2f", value)
+	}
+	a := Point(point).fmap(sprint_float)
+	assert.Equal(t, []string{"3.14", "3.14"}, a)
+	assert.Equal(t, "3.14,3.14", strings.Join(a, ","))
 }
